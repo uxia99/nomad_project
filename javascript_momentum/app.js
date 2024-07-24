3e68e0d86b24f6a984edbea0d7c07726497db8fe
장유정 <306406j@gmail.com>
2024-07-25 03:56:14 +0900

    Event II

--- javascript_momentum/app.js
+++ javascript_momentum/app.js
@@ -5,9 +5,18 @@ const loginInput = document.querySelector("#login-form input");
 // querySelector을 사용할 때는 ID를 명시하도록 #을 붙여줘야 함
 // getElementById는 명확히 Id를 찾기때문에 명시할 필요 없음
 
-function onLoginSubmit(potato) {
-  potato.preventDefault();
-  console.log(potato);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  potato.preventDefault(); // form 태그의 기본 동작을 막음
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault(); // a 태그의 기본 동작을 막음  
+  console.log(event);
+  alert("clicked!");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
