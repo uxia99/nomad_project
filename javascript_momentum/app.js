795561df6f851a06d8eae2a43f6a739c7f59cbff
장유정 <306406j@gmail.com>
2024-07-25 14:27:16 +0900

    localStorage_username  & Hidden

--- javascript_momentum/app.js
+++ javascript_momentum/app.js
@@ -9,17 +9,29 @@ const greeting = document.querySelector("#greeting");
 // 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고싶을 때 사용  
 // 혹은 loginForm, loginInput처럼 중요한 정보를 담은 게 아닐 경우 사용
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event
   .preventDefault(); // form 태그의 기본 동작을 막음
-  loginForm.classList.add("hidden");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username); 
+  
+  localStorage.setItem(USERNAME_KEY, username); // 좌 : key, 우 : value
 
   // greeting.innerText = "Hello, " + username; // 이렇게도 가능
   greeting.innerText = `Hello, ${username}`;
-  greeting.classList.remove("hidden");
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 } 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY); 
+console.log(savedUsername);
+
+if(savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello, ${savedUsername}`;
+}
