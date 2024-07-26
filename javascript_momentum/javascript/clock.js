522104304214464d5779b91bb1211d91f9f10c25
장유정 <306406j@gmail.com>
2024-07-26 16:04:06 +0900

    get clock

--- javascript_momentum/javascript/clock.js
+++ javascript_momentum/javascript/clock.js
@@ -1,7 +1,10 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello"); 
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; 
 }
 
-setInterval(sayHello, 5000); // 5초마다 sayHello 함수 실행
+getClock();
+setInterval(getClock, 1000); // 1초마다 getClock 함수 실행
+// setTimeout(sayHello, 5000); // 5초 후에 sayHello 함수 실행
