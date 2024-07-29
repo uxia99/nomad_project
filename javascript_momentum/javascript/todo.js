a28d7ba3c7b37325fefa6f0bdae6ea8d25427c02
장유정 <306406j@gmail.com>
2024-07-29 20:09:08 +0900

    Todo Span

--- javascript_momentum/javascript/todo.js
+++ javascript_momentum/javascript/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+  const list = document.createElement("li");
+  const span = document.createElement("span");
+  list.appendChild(span); // span을 li의 자식으로 추가
+  span.innerText = newToDo; // span의 text로 newToDo변수의 값을 넣음
+  console.log(list);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value; // input의 현재 value를 newToDo변수에 복사
   toDoInput.value = "";
+  paintToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
