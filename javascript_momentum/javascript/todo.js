5d9a72f3a98dbadd88c79d45b22f663b8269e1b2
장유정 <306406j@gmail.com>
2024-07-30 14:21:58 +0900

    Create <button> and check the console.log - what button

--- javascript_momentum/javascript/todo.js
+++ javascript_momentum/javascript/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) { // event를 받아서 해당 li에 대해서 delete를 실행
+  console.log(event.target.parentElement.innerText); // event.target은 button을 의미
+}
+
 function paintToDo(newToDo) {
   const list = document.createElement("li");
   const span = document.createElement("span");
-  list.appendChild(span); // span을 li의 자식으로 추가
   span.innerText = newToDo; // span의 text로 newToDo변수의 값을 넣음
+
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+
+  list.appendChild(span); // span을 li의 자식으로 추가
+  list.appendChild(button); // button을 li의 자식으로 추가
   toDoList.appendChild(list); // list를 ul의 자식으로 추가
 }
 
