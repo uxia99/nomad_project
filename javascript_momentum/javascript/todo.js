ce9a44e7aadc8f0e18d279486e30d6e37d41ee47
장유정 <306406j@gmail.com>
2024-07-30 14:02:18 +0900

    Made TodoList <ul>

--- javascript_momentum/javascript/todo.js
+++ javascript_momentum/javascript/todo.js
@@ -7,14 +7,14 @@ function paintToDo(newToDo) {
   const span = document.createElement("span");
   list.appendChild(span); // span을 li의 자식으로 추가
   span.innerText = newToDo; // span의 text로 newToDo변수의 값을 넣음
-  console.log(list);
+  toDoList.appendChild(list); // list를 ul의 자식으로 추가
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value; // input의 현재 value를 newToDo변수에 복사
   toDoInput.value = "";
-  paintToDo();
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
