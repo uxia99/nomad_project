8897a4b469d98242346768b55904e15fdccd99e5
장유정 <306406j@gmail.com>
2024-08-01 00:15:15 +0900

    Create City & Get Weather

---
+++ javascript_momentum/javascript/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "b6e4a8b65df24ee2d4442175b3072057";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Error getting location");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
