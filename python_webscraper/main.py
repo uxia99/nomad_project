341864d5f02c2c26d631e00a2cfc4ae37798ed4e
장유정 <306406j@gmail.com>
2024-08-07 03:11:24 +0900

    To Python Random Library

--- python_webscraper/main.py
+++ python_webscraper/main.py
@@ -1,7 +1,7 @@
-from random import random
+from random import randint
 
 user_choice = int(input("Choose number : "))
-pc_choice = random.randint(1,1000)
+pc_choice = randint(1,1000)
 
 if user_choice == pc_choice:
     print("Congratulations!")
@@ -9,3 +9,5 @@ elif user_choice > pc_choice:
     print("Lower")
 elif user_choice < pc_choice:
     print("Higher")
+
+print(f'The answer is : {pc_choice}')
--- python_webscraper/practice.py
+++ python_webscraper/practice.py
@@ -98,10 +98,10 @@ elif age < 20 or age >= 80:
 
 
 # Random Library Module 사용
-from random import random
+from random import randint
 
 user_choice = int(input("Choose number : "))
-pc_choice = random.randint(1, 1000)
+pc_choice = randint(1,1000)
 
 if user_choice == pc_choice:
     print("Congratulations!")
@@ -109,3 +109,5 @@ elif user_choice > pc_choice:
     print("Lower")
 elif user_choice < pc_choice:
     print("Higher")
+
+print(f'The answer is : {pc_choice}')
