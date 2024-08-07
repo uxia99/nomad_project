ef2912e64ce548e630461268681f36bba7fb4a26
장유정 <306406j@gmail.com>
2024-08-07 18:29:22 +0900

    To OOP_Methods

--- python_webscraper/main.py
+++ python_webscraper/main.py
@@ -1,8 +1,22 @@
 class Staff:
-    def __init__(self): # 모든 method는 첫 번째 argument로 그들 자신을 참조함
-        self.name = "Xia"
-        self.age = 25
-        self.team = "1Z Lab's"
+    # 데이터 세팅
+    def __init__(self, name, age, team): # 모든 method는 첫 번째 argument로 그들 자신을 참조함
+        self.name = name
+        self.age = age
+        self.team = team
 
-xia = Staff()
-print(xia.name, xia.age, xia.team)
+    # 데이터 접근
+    def __str__(self):
+        return f"Staff name : {self.name}, age : {self.age}, team : {self.team}"
+
+# Class의 인스턴스(instance)
+xia = Staff(
+    name = "Xia",
+    age = 25,
+    team = "1Z Lab's")
+jh = Staff(
+    name = "Jaeheon",
+    age = 27,
+    team = "ROKA")
+
+print(xia, jh)
--- python_webscraper/practice.py
+++ python_webscraper/practice.py
@@ -251,6 +251,27 @@ def introduce_player(player):
 introduce_player(xia)
 """
 
+class Staff:
+    # 데이터 세팅
+    def __init__(self, name, age, team): # 모든 method는 첫 번째 argument로 그들 자신을 참조함
+        self.name = name
+        self.age = age
+        self.team = team
 
+    # 데이터 접근
+    def __str__(self):
+        return f"Staff name : {self.name}, age : {self.age}, team : {self.team}"
+
+# Class의 인스턴스(instance)
+xia = Staff(
+    name = "Xia",
+    age = 25,
+    team = "1Z Lab's")
+jh = Staff(
+    name = "Jaeheon",
+    age = 27,
+    team = "ROKA")
+
+print(xia, jh)
 
 
